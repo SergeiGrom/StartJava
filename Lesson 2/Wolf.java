268931f0bd6e2b79ac06dc0938743c1809b89639
Sergei_Grom <morfeysg@gmail.com>
2024-04-10 13:39:06 +1000

    - изменения в Wolf и WolfTest согласно заданию

--- Lesson 2/Wolf.java
+++ Lesson 2/Wolf.java
@@ -1,9 +1,9 @@
 public class Wolf {
-    public String sex;
-    public String name;
-    public double weight;
-    public int age;
-    public String color;
+    private String sex;
+    private String name;
+    private double weight;
+    private int age;
+    private String color;
 
     public Wolf(String sex, String name, double weight, int age, String color) {
         this.sex = sex;
@@ -13,23 +13,67 @@ public class Wolf {
         this.color = color;
     }
 
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8.0) {
+            System.out.println(age + " Wrong age!");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public void move() {
-        System.out.println("Идти");;
+        System.out.println("Move");;
     }
 
     public void sit() {
-        System.out.println("Сидеть");
+        System.out.println("Sit");
     }
 
     public void run() {
-        System.out.println("Бежать");
+        System.out.println("Run");
     }
 
     public void howl() {
-        System.out.println("Выть");
+        System.out.println("Howl");
     }
 
     public void hunt() {
-        System.out.println("Охотиться");
+        System.out.println("Hunt");
     }
 }
--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -1,11 +1,17 @@
 public class WolfTest {
     public static void main(String[] args) {
-        Wolf whiteWolf = new Wolf("Male", "Beast", 20.5, 6, "White");
-        System.out.println("Sex: " + whiteWolf.sex);
-        System.out.println("Name: " + whiteWolf.name);
-        System.out.println("Weight: " + whiteWolf.weight);
-        System.out.println("Age: " + whiteWolf.age);
-        System.out.println("Color: " + whiteWolf.color);
+        Wolf whiteWolf = new Wolf("He-wolf", "Beast", 20.5, 6, "White");
+        System.out.println("Child Sex: " + whiteWolf.getSex());
+        whiteWolf.setSex("She-wolf");
+        System.out.println("New Sex: " + whiteWolf.getSex());
+        System.out.println("Name: " + whiteWolf.getName());
+        System.out.println("Weight: " + whiteWolf.getWeight());
+        System.out.println("Age: " + whiteWolf.getAge());
+        whiteWolf.setAge(9);
+        whiteWolf.setAge(1);
+        System.out.println("New Age: " + whiteWolf.getAge());
+        System.out.println("Color: " + whiteWolf.getColor());
+        System.out.println("\nWhiteWolf can: ");
         whiteWolf.move();
         whiteWolf.sit();
         whiteWolf.run();
