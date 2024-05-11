faac36ad3d31ec589caeb8a692a320ea750d38e3
Sergei_Grom <morfeysg@gmail.com>
2024-05-11 22:33:25 +1000

    - Lesson_4. Исправлены замечания.

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -6,127 +6,125 @@ import java.util.Arrays;
  * Задачи на тему: “Массивы. Цикл for-each”
  *
  * @author Sergei Grom
- * @version dated May 08, 2024
+ * @version dated May 11, 2024
  */
 
 public class ArrayTheme {
     public static void main(String[] args) {
-        arrayIndexReverse();
-        arrayCountFactorial();
-        arrayIndexDelete();
-        arrayAlphabetReverse();
-        arrayUniqueNumbers();
-        /*gameHangmanStart();
+        reverseVariables();
+        factorial();
+        deleteVariables();
+        reverseAlphabet();
+        addUniqueNums();
+        /* gameHangmanStart();
         typewriterTextOutput();*/
     }
 
-    public static void arrayIndexReverse() {
-        int[] src = {2, 1, 4, 3, 7, 6, 5};
-        int len = src.length;
+    public static void reverseVariables() {
+        int[] intNums = {2, 1, 4, 3, 7, 6, 5};
+        int len = intNums.length;
         System.out.println("1. Реверс значений массива");
-        System.out.println("До реверса: " + Arrays.toString(src));
-        for (int i = 0; src[i] != src[len - 1 - i]; i++) {
-            int temp = src[i];
-            src[i] = src[len - 1 - i];
-            src[len - 1 - i] = temp;
+        System.out.println("До реверса: " + Arrays.toString(intNums));
+        for (int i = 0; intNums[i] != intNums[len - 1 - i]; i++) {
+            int temp = intNums[i];
+            intNums[i] = intNums[len - 1 - i];
+            intNums[len - 1 - i] = temp;
         }
-        System.out.println("После реверса: " + Arrays.toString(src));
+        System.out.println("После реверса: " + Arrays.toString(intNums));
         System.out.println();
     }
 
-    public static void arrayCountFactorial() {
+    public static void factorial() {
         System.out.println("2. Вычисление факториала 8");
-        int[] src = new int[10];
-        int len = src.length;
+        int[] intNums = new int[10];
+        int len = intNums.length;
         int factorial = 1;
         for (int i = 0; i < len; i++) {
-            src[i] = i;
-            if (i == 0 || i == len -1) {
+            intNums[i] = i;
+            if (i == 0 || i == len - 1) {
                 continue;
             }
-            factorial *= src[i];
-            System.out.print(i != (len - 2) ? src[i] + " * " : src[i]);
+            factorial *= intNums[i];
+            System.out.print(i != (len - 2) ? intNums[i] + " * " : intNums[i]);
         }
         System.out.print(" = " + factorial);
         System.out.println("\n");
     }
 
-    public static void arrayIndexDelete() {
+    public static void deleteVariables() {
         System.out.println("3. Удаление элементов массива");
-        double[] src = new double[15];
-        int len = src.length;
+        double[] realNums = new double[15];
+        int len = realNums.length;
         int indexMiddle = 0;
         double numMiddle = 0;
-        System.out.println("Исходный массив : ");
         for (int i = 0; i < len; i++) {
-            src[i] = Math.random();
-            if (i == len / 2 + 1) {
-                System.out.println();
-                indexMiddle = i - 1;
-                numMiddle = src[i - 1];
+            realNums[i] = Math.random();
+            if (i == len / 2) {
+                indexMiddle = i;
+                numMiddle = realNums[i];
             }
-            System.out.printf("%.3f ", src[i]);
         }
-        System.out.printf("\nЗначение средней ячейки массива : %.3f \n", numMiddle);
+        System.out.printf("Значение средней ячейки массива : %.3f \n", numMiddle);
         System.out.println("Измененный массив : ");
         int numChange = 0;
         for (int i = 0; i < len; i++) {
-            if (src[i] > numMiddle) {
-                src[i] = 0.0;
+            if (realNums[i] > numMiddle) {
+                realNums[i] = 0.0;
                 numChange++;
             }
             if (i == indexMiddle + 1) {
                 System.out.println();
             }
-            System.out.printf("%.3f ", src[i]);
+            System.out.printf("%.3f ", realNums[i]);
         }
         System.out.println("\nКоличество изменений элементов массива : " + numChange);
     }
 
-    public static void arrayAlphabetReverse() {
+    public static void reverseAlphabet() {
         System.out.println("\n4. Вывод алфавита лесенкой");
-        char[] src = new char[26];
-        int len = src.length;
-        char letter = 'A';
+        char[] alphabet = new char[26];
+        int len = alphabet.length;
         for (int i = 0; i < len; i++) {
-            src [i] = letter++;
+            alphabet[i] = (char) ('A' + i);
         }
         for (int i = 0; i < len; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print(src[len - 1 - j]);
+                System.out.print(alphabet[len - 1 - j]);
             }
             System.out.println();
         }
     }
 
-    public static void arrayUniqueNumbers() {
+    public static void addUniqueNums() {
         System.out.println("\n5. Заполнение массива уникальными числами :");
-        int[] src = new int[39];
-        int len = src.length;
+        int[] uniqueNums = new int[39];
+        int len = uniqueNums.length;
+        int temp = 0;
         for (int i = 0; i < len; i++) {
             boolean flag = true;
             while (flag) {
                 flag = false;
-                src[i] = (int) (60 + Math.random() * 40);
+                temp = (int) (60 + Math.random() * 40);
                 for (int j = 0; j < i; j++) {
-                    if (src[j] == src[i]) {
+                    if (temp == uniqueNums[j]) {
                         flag = true;
                     }
                 }
             }
+            uniqueNums[i] = temp;
         }
         for (int i = 0; i < len; i++) {
             for (int j = 0; j <= i; j++) {
-                if (src[j] > src[i]) {
-                    int temp = src[j];
-                    src[j] = src[i];
-                    src[i] = temp;
+                if (uniqueNums[j] > uniqueNums[i]) {
+                    temp = uniqueNums[j];
+                    uniqueNums[j] = uniqueNums[i];
+                    uniqueNums[i] = temp;
                 }
             }
         }
         for (int i = 0; i < len - 1; i++) {
-            src[i] = src[i + 1];
-            System.out.print(src[i] + " ");
+            uniqueNums[i] = uniqueNums[i + 1];
+            System.out.print(uniqueNums[i] + " ");
             if ((i + 1) % 10 == 0) {
                 System.out.println();
             }
