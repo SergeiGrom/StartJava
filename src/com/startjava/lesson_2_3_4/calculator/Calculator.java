a15d23e5b0680636e3d6b6e903f98cb4097b4899
Sergei_Grom <morfeysg@gmail.com>
2024-07-17 11:42:43 +1000

    - Calculator внесены исправления.

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,10 +1,10 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
+    private static final int EXPRESSION_LENGTH = 3;
     private static int arg1;
     private static int arg2;
     private static String sign;
-    private static final int EXPRESSION_LENGTH = 3;
 
     private Calculator() {
         throw new AssertionError("This class cannot be instantiated");
@@ -25,26 +25,28 @@ public class Calculator {
     public static double calculate(String inputExpression) {
         String[] elements = inputExpression.trim().split(" +");
         checkExpressionLength(elements);
-        parseInt(elements);
+        parseArgs(elements);
         sign = elements[1];
         return switch (sign) {
             case "+" -> arg1 + arg2;
             case "-" -> arg1 - arg2;
             case "*" -> arg1 * arg2;
-            case "/" -> div(arg1, arg2);
+            case "/", "%" -> {
+                checkDivisor(arg2);
+                yield sign.equals("/") ? (double) arg1 / arg2 : Math.floorMod(arg1, arg2);
+            }
             case "^" -> Math.pow(arg1, arg2);
-            case "%" -> modulus(arg1, arg2);
             default -> throw new IllegalStateException("Ошибка! Используйте только операции - + * / % ^");
         };
     }
 
-    public static void checkExpressionLength(String[] elements) {
+    private static void checkExpressionLength(String[] elements) {
         if (elements.length != EXPRESSION_LENGTH) {
             throw new RuntimeException("Ошибка! Отделяйте пробелом каждый введенный элемент выражения.");
         }
     }
 
-    public static void parseInt(String[] elements) {
+    private static void parseArgs(String[] elements) {
         try {
             arg1 = Integer.parseInt(elements[0]);
             arg2 = Integer.parseInt(elements[2]);
@@ -53,18 +55,10 @@ public class Calculator {
         }
     }
 
-    public static double div(double arg1, double arg2) {
-        if (arg2 == 0.0) {
+    private static void checkDivisor(int divisor) {
+        if (divisor == 0) {
             throw new ArithmeticException("Ошибка! На 0 делить нельзя.");
         }
-        return arg1 / arg2;
-    }
-
-    public static double modulus(int arg1, int arg2) {
-        if (arg2 == 0.0) {
-            throw new ArithmeticException("Ошибка! На 0 делить нельзя.");
-        }
-        return Math.floorMod(arg1, arg2);
     }
 }
 
